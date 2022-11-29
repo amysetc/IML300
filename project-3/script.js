585c0f403bcf73a17e97baa6c2bcbf726e70d133
amysetc <aupham@usc.edu>
2022-11-28 23:31:10 -0800

    update

---
+++ project-3/script.js
@@ -0,0 +1,24 @@
+$('document').ready(function(){
+
+  $('.bio-1').hide();
+  $('.bio-2').hide();
+  $('.bio-3').hide();
+  $('.bio-4').hide();
+  $('.bio-5').hide();
+  $('.bio-6').hide();
+  $('.bio-7').hide();
+  $('.bio-8').hide();
+  $('.bio-9').hide();
+  $('.bio-10').hide();
+  $('.bio-11').hide();
+  $('.bio-12').hide();
+
+  $('.picture-1').click(function(){
+      $('#active').fadeOut(1000);
+      $('#active').removeAttr('id');
+    $('.bio-1').attr('id','active');
+    $('.bio-2').delay(1300);
+    $('.bio-2').fadeIn(1300);
+         })
+
+});
